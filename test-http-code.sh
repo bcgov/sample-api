5560cb673a7271161dead348f145d634f0cdec3d
Javier Cabrera <javcabr@gmail.com>
2024-08-31 20:00:10 -0700

    debuggint test code

--- test-http-code.sh
+++ test-http-code.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-
-if [ $( curl -s -o /dev/null -I -w "%{http_code}" http://0.0.0.0:3000 ) == 302 ]; then
+http_code=$(curl -s -o /dev/null -I -w "%{http_code}" http://0.0.0.0:3000)
+if [ $http_code == 302 ]; then
     echo "All good!" 
 else
     echo "Server is not up!"
+    echo "HTTP code is $http_code"
     exit 1
 fi
 
